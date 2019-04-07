f8a40001eabb598dec49f04ecacac9978f7a68b4
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-04-07 16:59:52 -0400

    jwt handling and sign in with password.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import SweetTweets from './components/SweetTweets'
 import SignIn from './components/SignIn'
-import TroopForm from './components/TroopForm'
-import SuccessPage from './components/SuccessPage'
+import SignUp from './components/SignUp'
 
 class App extends Component {
   render() {
@@ -11,9 +10,9 @@ class App extends Component {
       <>
         <Router>
           <Switch>
-            <Route exact path="/" component={SignIn} />
-            <Route exact path="/troop-form" component={TroopForm} />
-            <Route exact path="/success-page" component={SuccessPage} />
+            <Route exact path="/" component={SweetTweets} />
+            <Route exact path="/sign-in" component={SignIn} />
+            <Route exact path="/sign-up" component={SignUp} />
           </Switch>
         </Router>
       </>
